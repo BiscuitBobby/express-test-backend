7e9cdea2ea4adf969c22c7cda12b6025e24c708b
BiscuitBobby <87699062+BiscuitBobby@users.noreply.github.com>
2023-07-07 00:55:35 +0530

    routing

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ console.log("running")
 const express = require('express')
 const app = express()
 
-
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res/*, next*/) => {
     console.log('responce')
-    res.status(418).json({message: "I got no tea"})
-    res.render("index")
+    //res.status(418).json({message: "I got no tea"})
+    res.render("index", {text: "za warudo"})
 })
+
+const userRouters = require('./routes/users')
+app.use('/users', userRouters)
 
 app.listen(3000)
